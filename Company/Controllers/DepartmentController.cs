7726c147fec03f7396295259f63b52d9a2f978a8
aamirakhtar <aamiradvantage@gmail.com>
2020-05-28 05:54:57 +0500

    Add project files.

---
+++ Company/Controllers/DepartmentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Company.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public string Index()
+        {
+            return "Welcome faizan from Department/Index";
+        }
+
+        public ViewResult Welcome()
+        {
+            return View();
+        }
+
+        public string WelcomeWithDate()
+        {
+            return "Welcome Faizan" + DateTime.Now.ToString();
+        }
+
+        public ActionResult AddDepartment()
+        {
+            return View();
+        }
+    }
+}
