ab133449a7b3503a762fb4407d1e60a7a47454d6
aamirakhtar <aamiradvantage@gmail.com>
2020-05-31 06:41:56 +0500

    Lesson 2

--- Company/Controllers/DepartmentController.cs
+++ Company/Controllers/DepartmentController.cs
@@ -8,9 +8,14 @@ namespace Company.Controllers
 {
     public class DepartmentController : Controller
     {
-        public string Index()
+        public ActionResult Index()
         {
-            return "Welcome faizan from Department/Index";
+            CompanyEntities ce = new CompanyEntities();
+
+            //ViewBag.departments = ce.Departments.ToList();
+            ViewData["departments"] = ce.Departments.ToList();
+
+            return View();
         }
 
         public ViewResult Welcome()
@@ -27,5 +32,62 @@ namespace Company.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult AddDepartment(string name, string location, string departmentHead)
+        {
+            //Department is a data object which is made by Entity Framework. Its a db table snapshot converted into class
+            Department dept = new Department();
+            dept.Name = name;
+            dept.Location = location;
+            dept.DepartmentHead = departmentHead;
+
+            //Company Entities is a connection string
+            CompanyEntities ce = new CompanyEntities();
+
+            //System.Data.Entity.EntityState is crud operation enum
+            ce.Entry(dept).State = System.Data.Entity.EntityState.Added;
+            ce.SaveChanges();
+
+            return View();
+        }
+
+        //Parameterized
+        //public ActionResult AddDepartmentSubmit(string name, string location, string departmentHead)
+        //{
+        //    //Department is a data object which is made by Entity Framework. Its a db table snapshot converted into class
+        //    Department dept = new Department();
+        //    dept.Name = name;
+        //    dept.Location = location;
+        //    dept.DepartmentHead = departmentHead;
+
+        //    //Company Entities is a connection string
+        //    CompanyEntities ce = new CompanyEntities();
+
+        //    //System.Data.Entity.EntityState is crud operation enum
+        //    ce.Entry(dept).State = System.Data.Entity.EntityState.Added;
+        //    ce.SaveChanges();
+
+        //    return RedirectToAction("index");
+        //}
+
+        //Context Object
+        //public ActionResult AddDepartmentSubmit()
+        //{
+        //    //Department is a data object which is made by Entity Framework. Its a db table snapshot converted into class
+        //    Department dept = new Department();
+        //    dept.Name = Request.Form["name"];
+        //    dept.Location = Request.Form["location"];
+        //    dept.DepartmentHead = Request.Form["departmentHead"];
+
+        //    //Company Entities is a connection string
+        //    CompanyEntities ce = new CompanyEntities();
+
+        //    //System.Data.Entity.EntityState is crud operation enum
+        //    ce.Entry(dept).State = System.Data.Entity.EntityState.Added;
+        //    ce.SaveChanges();
+
+        //    return RedirectToAction("index");
+        //}
     }
 }
