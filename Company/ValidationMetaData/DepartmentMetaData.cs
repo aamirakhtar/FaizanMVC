d5140b72efd9acef0772f62e07e1cf00be65abe9
aamirakhtar <aamiradvantage@gmail.com>
2020-07-28 06:40:49 +0500

    Validation

---
+++ Company/ValidationMetaData/DepartmentMetaData.cs
@@ -0,0 +1,31 @@
+﻿using Company.ValidationMetaData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Company
+{
+    public class DepartmentMetaData
+    {
+        //[Display(Name = "Email")]
+        [MaxLength(20, ErrorMessage = "Name can be maximum of 10 characters.")]
+        [MinLength(2, ErrorMessage = "Name can be maximum of 10 characters.")]
+        [Required(ErrorMessage = "Please Enter Name.")]
+        [UniqueCheckValidation(ErrorMessage = "Department Alredy Exists.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Location.")]
+        public string Location { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Department Head.")]
+        public string DepartmentHead { get; set; }
+    }
+
+    [MetadataType(typeof(DepartmentMetaData))]
+    public partial class Department
+    {
+
+    }
+}
---
+++ Company/ValidationMetaData/UniqueCheckValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Company.ValidationMetaData
+{
+    public class UniqueCheckValidation : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            CompanyEntities ce = new CompanyEntities();
+
+            if (value != null && ce.Departments.ToList().Exists(d => d.Name == value.ToString()))
+                return false;
+            else
+                return true;
+        }
+    }
+}
